eb9f7c809f54641e72cb0efbd5f72667ce367f73
Adarsh <admaurya@deloitte.com>
2020-06-15 13:37:06 +0530

    Target- schemes and configuration changes

---
+++ General-CodeBase-iOS/OrangeGroup/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Orange-CodeBase-iOS
+//
+//  Created by Maurya, Adarsh on 15/06/20.
+//  Copyright © 2020 Adrsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
